8c66ad6421c4096b683d8b46415a52ea2d802871
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-07 16:15:55 +0100

    do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -7,17 +7,9 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/miuicamera-rubyx/miuicamera-rubyx-vendor.mk)
 
-# AIDL Interfaces
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.common-V4-ndk:64
-
 # Priv-app permission
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-miuicamera.xml
-
-# Shims
-PRODUCT_PACKAGES += \
-    libgui_shim_miuicamera
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
